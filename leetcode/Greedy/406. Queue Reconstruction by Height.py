79a46f37d8036ccf7f6ebb6bf5a13615eda9cc32
jinyes-kim <jinung@kakao.com>
2021-07-08 19:38:35 +0900

    2021.07.08.

--- leetcode/Greedy/406. Queue Reconstruction by Height.py
+++ leetcode/Greedy/406. Queue Reconstruction by Height.py
@@ -1,6 +1,10 @@
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-        heap = []
+	"""
+	순차 탐색은 아닌데, 뭔가 패턴이 보인다면 힙으로 정렬해보기
+	"""
+
+	heap = []
         for man in people:
             heapq.heappush(heap, (-man[0], man[1]))
         
@@ -10,4 +14,4 @@ class Solution:
             result.insert(man[1], [-man[0], man[1]])
         
         return result
-        
+       	
